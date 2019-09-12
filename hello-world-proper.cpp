b662686528f2eee5bf5070740a8ffc6866d44130
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-09-12 18:47:33 +0000

    program added

---
+++ hello-world-proper.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Amirmahdi Mersad All rights reserved
+//
+// Created by Amir Mersad
+// Created on September 2019
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+std::cout << "Hello, World I'm here." << std::endl;
+}
